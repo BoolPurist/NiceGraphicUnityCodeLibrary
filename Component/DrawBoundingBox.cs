776a7e20c301ee2c73da70d91488a7cb549e6031
BoolPurist <fnaumann@posteo.de>
2021-09-10 20:45:16 +0200

    Created component to draw bounding box
    Created function to get center of the volume
    of all meshes under a game object

---
+++ Component/DrawBoundingBox.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NiceGraphicsLibrary
+{
+  public class DrawBoundingBox : MonoBehaviour
+  {
+
+    [System.Serializable]
+    public class ElementColors
+    {
+      public Color Center = Color.red;
+      public Color Min = Color.blue;
+      public Color Max = Color.green;
+      public Color WireFrame = Color.grey;
+      public Color Diagonal = Color.cyan;
+    }
+
+    [SerializeField]
+    [Min(0)]
+    [Tooltip("Radius of a sphere which represents a point")]
+    private float RadiusSpherePoint = 0.1f;
+
+    [SerializeField]
+    private ElementColors Colors;
+
+    private void OnDrawGizmosSelected()
+    {      
+      Bounds boundingBox = Geometry3DUtility.GetVolumeBoundingBox(gameObject);
+      
+      Gizmos.color = Color.red;
+      Gizmos.DrawSphere(boundingBox.center, RadiusSpherePoint);
+      Gizmos.color = Colors.Min;
+      Gizmos.DrawSphere(boundingBox.min, RadiusSpherePoint);
+      Gizmos.color = Colors.Max;
+      Gizmos.DrawSphere(boundingBox.max, RadiusSpherePoint);
+      Gizmos.color = Colors.WireFrame;
+      Gizmos.DrawWireCube(boundingBox.center, boundingBox.size);
+      Gizmos.color = Colors.Diagonal;
+      Gizmos.DrawLine(boundingBox.min, boundingBox.max);
+    }
+  }
+
+}
---
+++ Utility/Geometry3DUtility.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NiceGraphicsLibrary
+{
+  public static class Geometry3DUtility
+  {
+    /// <summary>
+    /// Gets the center, size etc from a whole mesh of an object. Whole mesh can made up of several meshes
+    /// residing in the object itself and its children
+    /// </summary>
+    /// <param name="rootObject">
+    /// Object to get the bounding box from
+    /// </param>
+    /// <return>
+    /// Returns the bounding box of the whole mesh
+    /// </return>
+    /// <remarks>
+    /// To work, object with a mesh needs the component MeshRenderer
+    /// </remarks>
+    public static Bounds GetVolumeBoundingBox(GameObject rootObject)
+    {
+      float minX = float.MaxValue;
+      float maxX = float.MinValue;
+      float minY = float.MaxValue;
+      float maxY = float.MinValue;
+      float minZ = float.MaxValue;
+      float maxZ = float.MinValue;
+
+      CompareOneMesh(rootObject);
+
+      var bounding = new Bounds()
+      {
+        max = new Vector3(maxX, maxY, maxZ),
+        min = new Vector3(minX, minY, minZ)
+      };
+
+      return bounding;
+
+      void CompareOneMesh(GameObject oneObject)
+      {
+        var meshRenderer = oneObject.GetComponent<MeshRenderer>();
+
+        if (meshRenderer != null)
+        {
+          Bounds boundingBox = meshRenderer.bounds;
+          minX = Mathf.Min(minX, boundingBox.min.x);
+          maxX = Mathf.Max(maxX, boundingBox.max.x);
+          minY = Mathf.Min(minY, boundingBox.min.y);
+          maxY = Mathf.Max(maxY, boundingBox.max.y);
+          minZ = Mathf.Min(minZ, boundingBox.min.z);
+          maxZ = Mathf.Max(maxZ, boundingBox.max.z);
+        }
+
+        for (int i = 0; i < oneObject.transform.childCount; i++)
+        {
+          CompareOneMesh(oneObject.transform.GetChild(i).gameObject);
+        }
+      }
+    }
+  }
+
+}
