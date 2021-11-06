eca1fdf2cf6f0c0675c95fe79afa15ee55d73cce
BoolPurist <fnaumann@posteo.de>
2021-11-06 23:39:20 +0100

    Created component for an object to follow mouse cursor.

--- Assets/PackageNicegraphicLibrary/Runtime/ITakesGameInputProviderProvider.cs
+++ Assets/PackageNicegraphicLibrary/Runtime/ITakesGameInputProviderProvider.cs
@@ -5,8 +5,17 @@ using UnityEngine;
 
 namespace NiceGraphicLibrary
 {
+  /// <summary>
+  /// Interface for taking implementation for game getting 
+  /// </summary>
   public interface ITakesGameInputProviderProvider
   {
+    /// <summary>
+    /// Used to change the implementation for getting input from the player in a game.
+    /// </summary>
+    /// <param name="newProvider">
+    /// New implementation for getting game input.
+    /// </param>
     public void SetKeyButtonProvider(IGameInputProvider newProvider);
   }
 }
