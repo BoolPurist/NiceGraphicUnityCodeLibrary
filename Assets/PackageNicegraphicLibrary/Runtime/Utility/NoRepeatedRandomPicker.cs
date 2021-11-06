9588296b4fcd6480a956754ad3b924fdd6aefae9
BoolPurist <fnaumann@posteo.de>
2021-11-06 22:28:45 +0100

    Commented NoRepeatedRandomPicker and added test for it.

--- Assets/PackageNicegraphicLibrary/Runtime/Utility/NoRepeatedRandomPicker.cs
+++ Assets/PackageNicegraphicLibrary/Runtime/Utility/NoRepeatedRandomPicker.cs
@@ -6,6 +6,13 @@ using UnityEngine;
 
 namespace NiceGraphicLibrary.Utility
 {
+  /// <summary>
+  /// Instance to pick random elements of a sequence in a way that random element is not returned again 
+  /// until all elements of given sequence are returned.
+  /// </summary>
+  /// <typeparam name="TElement">
+  /// Type of a random element of the sequence to pick from
+  /// </typeparam>
   public class NoRepeatedRandomPicker<TElement>
   {
 #pragma warning disable IDE0090 // Use 'new(...)'
@@ -13,10 +20,20 @@ namespace NiceGraphicLibrary.Utility
 #pragma warning restore IDE0090 // Use 'new(...)'
     private int _currentIndex = -1;
 
+    /// <inheritdoc cref="SetANew(IEnumerable{TElement})" />
     public NoRepeatedRandomPicker(IEnumerable<TElement> collectionToPickFrom)
     {
       SetANew(collectionToPickFrom);
     }
+
+    /// <summary>
+    /// Returns a random value from the a given sequence. 
+    /// The returned random value does not come from are already given index until all elements of the sequence were returned
+    /// </summary>
+    /// <returns>
+    /// Returns a random value. 
+    /// Note: if the given sequence is empty or null then default value of the type of an element is returned
+    /// </returns>
 
     public TElement Next
     {
@@ -33,20 +50,30 @@ namespace NiceGraphicLibrary.Utility
             Reset();
           }
 
-          _currentIndex++;
           TElement nextElement = _innerCollection[_currentIndex];
+          _currentIndex++;
 
           return nextElement;
         }
       }
     }
 
+    /// <summary>
+    /// Resets the given sequence so the random elements are picked as if the no element was picked before.
+    /// </summary>
     public void Reset()
     {
       RandomUtility.Shuffle(_innerCollection);
       _currentIndex = 0;
     }
 
+    /// <summary>
+    /// Changes given sequence to choose random elements from.
+    /// </summary>
+    /// <param name="collectionToPickFrom">
+    /// New given sequence for picking random elements.
+    /// If null then the old given sequence is kept for picking
+    /// </param>
     public void SetANew(IEnumerable<TElement> collectionToPickFrom)
     {
       if (collectionToPickFrom != null)
